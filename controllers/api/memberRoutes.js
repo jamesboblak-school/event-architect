19af9c728962b430158e2c87eae01969bfae7196
jamesboblak <james@skepticalrecords.com>
2021-08-05 17:24:50 -0700

    added router to memberRoutes.js

--- controllers/api/memberRoutes.js
+++ controllers/api/memberRoutes.js
@@ -1,3 +1,17 @@
+// associate the models in variables
+const router = require('express').Router();
+// const {
+//     Member,
+//     Message,
+//     Detail,
+//     Event,
+//     //   CHECK '_' in Member_event !!!!!!! CHECK
+//     Member_event,
+//     //   CHECK '_' in Member_member !!!!!!! CHECK
+//     Member_member
+// } = require('../models');
+
+
 //   ============USER ROUTES============
 // 2. Become a Member POST
 router.post('/', (req, res) => {
